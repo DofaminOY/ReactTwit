4b8bfe1ad799d7fd8fa0df7c11d2fcfb53a4ac11
Yura Ostrovsky <yurkasoft@gmail.com>
2020-12-16 19:54:04 +0200

    add SearchPanel

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
 
 
 const App = () => {
     return (
+        <div className="app">
         <AppHeader/>
+        <div className = "serc-panel d-flex">
+         <SearchPanel/>
+        </div>
+        </div>
     )
 }
 export default App;
