87c198cdc51bf4f55f09837235c87f562bdad9d2
Yura Ostrovsky <yurkasoft@gmail.com>
2020-12-16 23:28:56 +0200

    correct PostStatusFilter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+
 
 
 const App = () => {
     return (
-        <div className="app">
+     <div className="app">
         <AppHeader/>
-        <div className = "serc-panel d-flex">
-         <SearchPanel/>
+          <div className = "serc-panel d-flex">
+          <SearchPanel/>
+          <PostStatusFilter/>
         </div>
-        </div>
+     </div>
     )
 }
 export default App;
