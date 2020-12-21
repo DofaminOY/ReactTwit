9572b9456321fda0e8312dd18d3dfc530ada4a32
Yura Ostrovsky <yurkasoft@gmail.com>
2020-12-21 22:34:45 +0200

    postAddForm

---
+++ src/components/post-add-form/post-add-form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './post-add-form.css';
+const PostAddForm = () => {
+    return (
+        <div className = "bottom-panel d-flex">
+            <input
+
+            type = "text"
+            placeholder = "О чем вы думаете сейчас?"
+            className = "form-control new-post-label"
+            />
+            <button
+            type="submit"
+            className = "btn btn-outline-secondary">
+                Добавить</button>
+
+        </div>
+    )
+}
+export default PostAddForm;
