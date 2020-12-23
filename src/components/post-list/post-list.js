ec5e78a43f310eab3b9b2f925566bad006aad604
Yura Ostrovsky <yurkasoft@gmail.com>
2020-12-23 21:40:23 +0200

    + onImportant & onLike

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,14 +3,32 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+    const elements = posts.map( (item) => {
+        
+        if ( typeof item === 'object' && isEmpty(item) ){ 
+            const {id, ...itemProps} = item;
+            return (
+                <li key = {id} className='list-group-item'>
+                    <PostListItem {...itemProps}/>
+                </li>
+            )
+        }
+    })
+
+    function isEmpty(obj) {
+        for(let key in obj)
+        {
+            return true;
+        }
+        return false;
+    }
+
     return (
-        <ul className = "app-list list-group">
-            <PostListItem lable = "Latina Latin Edition concedit quod est efficiens" />
-            <PostListItem lable = "Primum, in user ad inserere in hoc"/>
-            <PostListItem lable = "Per claves ad respondentem, in lingua enim indicavit fonte, tum in scopum lingua"/>
+        <ul className="app-list list-group">
+           {elements}
         </ul>
     )
 }
-
 export default PostList;
