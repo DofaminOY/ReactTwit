ef2ddcb4801ca1882deb816ffad21b452328576e
Yura Ostrovsky <yurkasoft@gmail.com>
2020-12-24 21:40:30 +0200

    add onDelete postlIST

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,34 +1,26 @@
 import React from 'react';
 
 import PostListItem from '../post-list-item';
-import './post-list.css';
+import './post-list.css'
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
-    const elements = posts.map( (item) => {
-        
-        if ( typeof item === 'object' && isEmpty(item) ){ 
-            const {id, ...itemProps} = item;
-            return (
-                <li key = {id} className='list-group-item'>
-                    <PostListItem {...itemProps}/>
-                </li>
-            )
-        }
-    })
-
-    function isEmpty(obj) {
-        for(let key in obj)
-        {
-            return true;
-        }
-        return false;
-    }
+    const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
+        return (
+            <li key={id} className='list-group-item'>
+                <PostListItem 
+                {...itemProps}
+                onDelete={() => onDelete(id)}/>
+            </li>
+        )
+    });
 
     return (
         <ul className="app-list list-group">
-           {elements}
+            {elements}
         </ul>
     )
 }
+
 export default PostList;
